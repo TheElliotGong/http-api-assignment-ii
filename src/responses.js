3d25840f92c8adb122cdd45dcae20778625ac439
thetyper101 <ewcgong@hotmail.com>
2023-09-26 12:43:00 -0400

    edited and added functions

--- src/responses.js
+++ src/responses.js
@@ -1,7 +1,75 @@
+const users = {};
+
 const respondJSON = (request, response, status, content) => {
-    response.writeHead(status, { 'Content-Type': 'application/json'});
-    response.write(content);
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(content);
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
   };
 
-  module.exports = {respond};
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  // Default status code for updating existing users
+  let responseCode = 204;
+
+  // If the user doesn't exist yet
+  if (!users[body.name]) {
+    // Set the status code to 201 (created) and create an empty user
+    responseCode = 201;
+    users[body.name] = {};
+  }
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  // 204 has an empty payload, just a success
+  // It cannot have a body, so we just send a 204 without a message
+  // 204 will not alter the browser in any way!!!
+  return respondJSONMeta(request, response, responseCode);
+};
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => {
+  respondJSONMeta(request, response, 200);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
+module.exports = {
+  respondJSON,
+  respondJSONMeta,
+  addUser,
+  getUsers,
+  getUsersMeta,
+  notFound,
+  notFoundMeta,
+};
--- src/server.js
+++ src/server.js
@@ -4,28 +4,60 @@ const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const responseHandler = require('./responses.js');
 
-const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    const params = query.parse(parsedUrl.query);
-    const acceptedType = request.headers.accept;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-    switch(parsedUrl.pathname)
-    {
-        case '/':
-            htmlHandler.getIndex(request, response);
-            break;
-        case '/style.css':
-            htmlHandler.getCSS(request, response);
-            break;
-        default:
-            responseHandler.respond(request, response, 404, {message: 'The page you were looking for was not found.',
-            id: 'notFound'});
-            break;
-    }
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': responseHandler.getUsers,
+    '/notReal': responseHandler.notFound,
+    notFound: responseHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': responseHandler.getUsersMeta,
+    '/notReal': responseHandler.notFoundMeta,
+    notFound: responseHandler.notFoundMeta,
+  },
+  POST: { '/addUser': responseHandler.addUser },
 };
 
+const parseBody = (request, response, callback) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (data) => {
+    body.push(data);
+  });
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    // Once we have the bodyParams object, we will call the handler function. We then
+    // proceed much like we would with a GET request.
+    callback(request, response, bodyParams);
+  });
+};
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, urlStruct.POST.addUser);
+  }
+};
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'GET' || request.method === 'HEAD') {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  }
+};
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
-  });
-  
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
