259ca1a9f28383a932c9027941963934c9283523
thetyper101 <ewcgong@hotmail.com>
2023-09-26 15:23:32 -0400

    added functions to handle different request types

--- src/server.js
+++ src/server.js
@@ -45,16 +45,57 @@ const parseBody = (request, response, callback) => {
 };
 const handlePost = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/addUser') {
-    parseBody(request, response, urlStruct.POST.addUser);
+    parseBody(request, response, responseHandler.addUser);
   }
 };
+
+const handleGet = (request, response, parseUrl) => {
+  switch(parseUrl.pathname) 
+  {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      htmlHandler.getCSS(request, response);
+      break;
+    case '/getUsers':
+      responseHandler.getUsers(request, response);
+      break;
+    case '/notReal':
+      responseHandler.notFound(request, response);
+      break;
+    default:
+      responseHandler.notFound(request, response);
+      break;
+  }
+};
+
+const handleHead = (request, response, parseUrl) => {
+switch(parseUrl.pathname) 
+{
+  case '/getUsers':
+    responseHandler.getUsersMeta(request, response);
+    break;
+  case '/notReal':
+    responseHandler.notFoundMeta(request, response);
+    break;
+  default:
+    responseHandler.notFoundMeta(request, response);
+    break;
+}
+}
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-
+  
+  console.log(request.method);
   if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
-  } else if (request.method === 'GET' || request.method === 'HEAD') {
-    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else if (request.method === 'GET') {
+    handleGet(request, response, parsedUrl);
+  }
+  else if(request.method === 'HEAD')
+  {
+    handleHead(request, response, parsedUrl);
   }
 };
 
