9213d3f2aa84e248434ac05aba1fea1535297744
thetyper101 <ewcgong@hotmail.com>
2023-09-30 16:56:08 -0400

    Added comments

--- src/responses.js
+++ src/responses.js
@@ -1,20 +1,38 @@
 const users = {};
-
+/**
+ * This helper function returns given data in the desired format, used for get responses.
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} status The http status code for this response
+ * @param {*} content The data to return in the object.
+ */
 const respondJSON = (request, response, status, content) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(content));
   response.end();
 };
-
+/**
+ * This helper function returns meta data in the desired format, used for head responses.
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} status 
+ */
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
 };
+/**
+ * This function adds a user to the users object, or updates an existing user.
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} body the data needed to add or update a user.
+ * @returns 
+ */
 const addUser = (request, response, body) => {
   const responseJSON = {
     message: 'Name and age are both required.',
   };
-
+  //Check if data is valid.
   if (!body.name || !body.age) {
     responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
@@ -31,38 +49,54 @@ const addUser = (request, response, body) => {
   //Update or add fields for this user
   users[body.name].name = body.name;
   users[body.name].age = body.age;
-
+  //Response for when a user is created
   if (responseCode === 201) {
     responseJSON.message = 'Created Successfully';
     return respondJSON(request, response, responseCode, responseJSON);
   }
-  // 204 has an empty payload, just a success
-  // It cannot have a body, so we just send a 204 without a message
-  // 204 will not alter the browser in any way!!!
+  //Response for when a user is updated
   return respondJSONMeta(request, response, responseCode);
 };
+/**
+ * This function returns the saved list of users from the server.
+ * @param {*} request 
+ * @param {*} response 
+ */
 const getUsers = (request, response) => {
   const responseJSON = {users,};
   respondJSON(request, response, 200, responseJSON);
 };
-
+/**
+ * This function returns the meta data for the list of users from the server.
+ * @param {*} request 
+ * @param {*} response 
+ */
 const getUsersMeta = (request, response) => {
   respondJSONMeta(request, response, 200);
 };
+/**
+ * This function returns a not found error, complete with a message.
+ * @param {*} request 
+ * @param {*} response 
 
+ */
 const notFound = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
   };
 
-  return respondJSON(request, response, 404, responseJSON);
+  respondJSON(request, response, 404, responseJSON);
 };
-
+/**
+ * This function returns the meta data for a not found error.
+ * @param {*} request 
+ * @param {*} response 
+ */
 const notFoundMeta = (request, response) => {
   respondJSONMeta(request, response, 404);
 };
-
+//Export functions
 module.exports = {
   respondJSON,
   respondJSONMeta,
